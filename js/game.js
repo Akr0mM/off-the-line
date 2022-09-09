e97f294e3c82062b5e7112126f288606f788ba9c
Maxime Faivre-Maçon <maxime.fm@gmail.com>
2022-09-09 20:02:25 +0200

    create map 1

--- js/game.js
+++ js/game.js
@@ -1,50 +1,45 @@
-import { Menu } from './menu.js'
-import { Map } from './map.js'
+import { Menu } from './menu.js';
+import { Map } from './map.js';
+import { Player } from './player.js';
 
 
 
-const cw = 640
-const ch = 480
-const canvas = document.getElementById('canvas') 
-const ctx = canvas.getContext("2d")
-canvas.width = cw
-canvas.height = ch
+const cw = 640;
+const ch = 480;
+const canvas = document.querySelector('canvas');
+const ctx = canvas.getContext("2d");
+canvas.style = "width: 640; height: 480; background-color: black";
+canvas.width = cw;
+canvas.height = ch;
 
-let menu 
-let map 
-let gameReady = true
-let gameLaunched = false
+let menu;
+let map;
+let gameReady = true;
+let gameLaunched = false;
 
 window.onload = function main() {
-    map = new Map(ctx, canvas, 0)
-    menu = new Menu(canvas, ctx, gameReady)
+    map = new Map(ctx, canvas, 0);
+    menu = new Menu(canvas, ctx, gameReady);
 
-    animate()
-}
+    animate();
+};
 
 function animate() {
-    update()
-    draw()
+    update();
+    draw();
 
-    requestAnimationFrame(animate)
+    requestAnimationFrame(animate);
 }
 
 function update() {
-    if (gameReady) gameLaunched = true
+    if (gameReady) gameLaunched = true;
 
 
 }
 
 function draw() {
-    if (!gameLaunched) return 
-    
-    // bg 
-    ctx.fillStyle = '#121212'
-    ctx.fillRect(0, 0, cw, ch)
+    if (!gameLaunched) return;
 
     // map 
-    map.draw()
-
-    // coins
-    map.coins.draw()
+    map.draw();
 }
--- js/map.js
+++ js/map.js
@@ -1,36 +1,58 @@
 export class Map {
     constructor(ctx, canvas, levelIndex) {
-        this.ctx = ctx
-        this.canvas = canvas
-        this.levelIndex = levelIndex
-        this.patternColor = '#eee'
-        this.xStart = (canvas.width - 200) / 2
-        this.yStart = (canvas.height - 200) / 2
-        this.drawMap = {
-            '0': () => {
-                ctx.strokeStyle = this.patternColor
-                ctx.rect(this.xStart, this.yStart, 200, 200)
-                ctx.stroke()
-            }
-        }
+        this.ctx = ctx;
+        this.canvas = canvas;
+        this.levelIndex = levelIndex;
+        this.patternColor = '#fff';
+        this.xStart;
+        this.yStart;
         this.coins = {
-            length: 15,
-            placement: [{x: this.xStart + 40, y: this.yStart + 40}],
-            draw: () => this.drawCoins(),
+            length: 0,
+            placement: [],
             color: 'yellow',
             width: 8
-        }
+        };
     }
 
-    draw() {  
-        if (this.levelIndex === 0) this.drawMap['0']()
+    draw() {
+        // this.drawBackground()
+
+        if (this.levelIndex === 0) {
+            // pattern
+            this.xStart = (this.canvas.width - 200) / 2;
+            this.yStart = (this.canvas.height - 200) / 2;
+            this.ctx.strokeStyle = this.patternColor;
+            this.ctx.strokeRect(this.xStart, this.yStart, 200, 200);
+
+            // coins 
+            this.coins.placement.push({ x: this.xStart + 44, y: this.yStart + 44 });
+            this.coins.placement.push({ x: this.xStart + 70, y: this.yStart + 44 });
+            this.coins.placement.push({ x: this.xStart + 96, y: this.yStart + 44 });
+            this.coins.placement.push({ x: this.xStart + 122, y: this.yStart + 44 });
+            this.coins.placement.push({ x: this.xStart + 148, y: this.yStart + 44 });
+
+            this.coins.placement.push({ x: this.xStart + 44, y: this.yStart + 96 });
+            this.coins.placement.push({ x: this.xStart + 70, y: this.yStart + 96 });
+            this.coins.placement.push({ x: this.xStart + 96, y: this.yStart + 96 });
+            this.coins.placement.push({ x: this.xStart + 122, y: this.yStart + 96 });
+            this.coins.placement.push({ x: this.xStart + 148, y: this.yStart + 96 });
+            this.coins.placement.push({ x: this.xStart + 44, y: this.yStart + 148 });
+            this.coins.placement.push({ x: this.xStart + 70, y: this.yStart + 148 });
+            this.coins.placement.push({ x: this.xStart + 96, y: this.yStart + 148 });
+            this.coins.placement.push({ x: this.xStart + 122, y: this.yStart + 148 });
+            this.coins.placement.push({ x: this.xStart + 148, y: this.yStart + 148 });
+            this.drawCoins();
+        }
     }
 
     drawCoins() {
-        this.coins.placement.forEach(coin => this.drawCoin(coin.x, coin.y))
+        this.coins.placement.forEach(coin => {
+            this.ctx.strokeStyle = this.coins.color;
+            this.ctx.strokeRect(coin.x, coin.y, this.coins.width, this.coins.width);
+        });
     }
 
-    drawCoin(x, y) {
-        
+    drawBackground() {
+
     }
 }
